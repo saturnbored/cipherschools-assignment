c015663783dd6a86722c7134624dd515873c16b4
Shivam Ruhil <shivamruhilh@gmail.com>
2023-04-08 18:10:47 +0530

    add user register/login routes and controllers

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const { connectDB } = require("./config/db");
+const { notFound, errorHandler } = require("./middlewares/Errors");
+const userRoutes = require("./routes/User");
+
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/user", userRoutes);
+
+// Error handling
+app.use(notFound);
+app.use(errorHandler);
+
+// Start server
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
